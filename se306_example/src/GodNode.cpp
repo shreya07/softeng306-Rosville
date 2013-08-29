43a70c6ff95a8b32ec07f44e5990d5eb3cba359d
Surbhi <surbhi@ubuntu.(none)>
2013-08-29 16:57:53 -0400

    adding modified Godnode to accompany poop changes

--- se306_example/src/GodNode.cpp
+++ se306_example/src/GodNode.cpp
@@ -20,7 +20,14 @@ ros::Publisher BlockOne_StageOdom_pub;//To Sheep
 ros::Publisher BlockOne_StageLaser_pub; //To Sheep
 ros::Publisher BlockOne_cmd_vel_pub; //To stage
 
+    
+    //Publishers for PoopOne
+ros::Publisher PoopOne_StageOdom_pub;//To Poop
+ros::Publisher PoopOne_StageLaser_pub; //To Poop
+ros::Publisher PoopOne_cmd_vel_pub; //To stage
+ros::Publisher poopAdvert; //to poop
 
+    
 //-----[END]Publisher variables
 
 //-----Subscriber callbacks
@@ -36,7 +43,13 @@ void BlockOne_StageOdom_callback(nav_msgs::Odometry msg){ BlockOne_StageOdom_pub
 void BlockOne_StageLaser_callback(sensor_msgs::LaserScan msg){BlockOne_StageLaser_pub.publish(msg);}
 void BlockOne_cmd_vel_callback (geometry_msgs::Twist msg){BlockOne_cmd_vel_pub.publish(msg);}
 
+    
+//Callbacks for PoopOne
+void PoopOne_StageOdom_callback(nav_msgs::Odometry msg){ PoopOne_StageOdom_pub.publish(msg);}
+void PoopOne_StageLaser_callback(sensor_msgs::LaserScan msg){PoopOne_StageLaser_pub.publish(msg);}
+void PoopOne_cmd_vel_callback (geometry_msgs::Twist msg){PoopOne_cmd_vel_pub.publish(msg);}
 
+    
 //-----[END] Subscriber callbacks
 
 ros::Publisher Weather_publisher;//To Sheep
@@ -44,13 +57,13 @@ ros::Publisher Weather_publisher;//To Sheep
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "GodNode");
+	ros::init(argc, argv, "GodNode");
 
-  //NodeHandle is the main access point to communicate with ros.
-  ros::NodeHandle n;
+	//NodeHandle is the main access point to communicate with ros.
+	ros::NodeHandle n;
 
-  // Advertisements.....
-  
+	// Advertisements.....
+	
     //Advertisments for SheepOne
     SheepOne_StageOdom_pub = n.advertise<nav_msgs::Odometry>("SheepOne/odom", 1000); //To SheepOne
     SheepOne_StageLaser_pub= n.advertise<sensor_msgs::LaserScan>("SheepOne/base_scan", 1000); //To SheepOne
@@ -63,10 +76,17 @@ int main(int argc, char **argv)
     BlockOne_cmd_vel_pub=n.advertise<geometry_msgs::Twist>("robot_2/cmd_vel", 1000); //To stage
     
     
+    //Advertisments for PoopOne
+    PoopOne_StageOdom_pub = n.advertise<nav_msgs::Odometry>("PoopOne/odom", 1000); //To PoopOne
+    PoopOne_StageLaser_pub= n.advertise<sensor_msgs::LaserScan>("PoopOne/base_scan", 1000); //To PoopOne
+    PoopOne_cmd_vel_pub=n.advertise<geometry_msgs::Twist>("robot_3/cmd_vel", 1000); //To stage
+    poopAdvert = n.advertise<std_msgs::String>("poop", 1000); //to poop topic
+    
+    
 
 
-  //Setting up subscribers...
-  
+	//Setting up subscribers...
+	
     //Subscriber for SheepOne
     ros::Subscriber SheepOne_StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_1/odom", 1000, SheepOne_StageOdom_callback);
     ros::Subscriber SheepOne_StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_1/base_scan", 1000, SheepOne_StageLaser_callback);
@@ -77,41 +97,60 @@ int main(int argc, char **argv)
     ros::Subscriber BlockOne_StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_2/odom", 1000, BlockOne_StageOdom_callback);
     ros::Subscriber BlockOne_StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_2/base_scan", 1000, BlockOne_StageLaser_callback);
     ros::Subscriber BlockOne_cmd_vel = n.subscribe<geometry_msgs::Twist>("BlockOne/cmd_vel", 1000,BlockOne_cmd_vel_callback);
+    
+    
+    //Subscriber for PoopOne
+    ros::Subscriber PoopOne_StageOdo_sub = n.subscribe<nav_msgs::Odometry>("robot_3/odom", 1000, PoopOne_StageOdom_callback);
+    ros::Subscriber PoopOne_StageLaser_sub = n.subscribe<sensor_msgs::LaserScan>("robot_3/base_scan", 1000, PoopOne_StageLaser_callback);
+    ros::Subscriber PoopOne_cmd_vel = n.subscribe<geometry_msgs::Twist>("PoopOne/cmd_vel", 1000,PoopOne_cmd_vel_callback);
+    
+    
 
+	Weather_publisher = n.advertise<std_msgs::String>("weather/status", 1000); //To SheepOne
 
+	ros::Rate loop_rate(10);
 
-  Weather_publisher = n.advertise<std_msgs::String>("weather/status", 1000); //To SheepOne
+	//a count of howmany messages we have sent
+	int count = 0;
 
-  ros::Rate loop_rate(10);
+	//send to poop class requesting poop to drop;
+	std_msgs::String poopInfo;
 
-  //a count of howmany messages we have sent
-  int count = 0;
+	int poopCount = 0;
 
-  ////messages
-  //velocity of this RobotNode
-  std_msgs::String status;
-  status.data="Sunny";
-  while (ros::ok())
-  {
-    if (count%30==0)
-        {
-          status.data="Rainy";
-          Weather_publisher.publish(status);
+	////messages
+	//velocity of this RobotNode
+	std_msgs::String status;
+	status.data="Sunny";
+	while (ros::ok())
+	{
+		if (count%30==0)
+		{
+			status.data="Rainy";
+			Weather_publisher.publish(status);
 
-         // ROS_INFO("rainy");
-        }else if (count%59){
-          status.data="Sunny";
-          //ROS_INFO("sunny");
-          Weather_publisher.publish(status);
+			//ROS_INFO("rainy");
+		}else if (count%59){
+			status.data="Sunny";
+			//ROS_INFO("sunny");
+			Weather_publisher.publish(status);
 
-        }
+		}
 
-        ros::spinOnce();
+		if (count % 100) {
+			if (poopCount < 5) {
+				poopInfo.data = "Request";
+				poopAdvert.publish(poopInfo);
+			}
+			poopCount++;
+		}
 
-        loop_rate.sleep();
-        count=count+1;
-  }
+		ros::spinOnce();
 
-  return 0;
+		loop_rate.sleep();
+		count=count+1;
+	}
+
+	return 0;
 
 }
--- se306_example/src/GodNodeTemplate.cpp
+++ se306_example/src/GodNodeTemplate.cpp
@@ -20,50 +20,63 @@ ros::Publisher Weather_publisher;//To Sheep
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "GodNode");
+	ros::init(argc, argv, "GodNode");
 
-  //NodeHandle is the main access point to communicate with ros.
-  ros::NodeHandle n;
+	//NodeHandle is the main access point to communicate with ros.
+	ros::NodeHandle n;
 
-  // Advertisements.....
-  //RegisterPublisher
+	// Advertisements.....
+	//RegisterPublisher
 
 
-  //Setting up subscribers...
-  //SetupSubscribers
+	//Setting up subscribers...
+	//SetupSubscribers
 
-  Weather_publisher = n.advertise<std_msgs::String>("weather/status", 1000); //To SheepOne
+	Weather_publisher = n.advertise<std_msgs::String>("weather/status", 1000); //To SheepOne
 
-  ros::Rate loop_rate(10);
+	ros::Rate loop_rate(10);
 
-  //a count of howmany messages we have sent
-  int count = 0;
+	//a count of howmany messages we have sent
+	int count = 0;
 
-  ////messages
-  //velocity of this RobotNode
-  std_msgs::String status;
-  status.data="Sunny";
-  while (ros::ok())
-  {
-    if (count%30==0)
-        {
-          status.data="Rainy";
-          Weather_publisher.publish(status);
+	//send to poop class requesting poop to drop;
+	const std_msgs::String::ConstPtr& poopInfo;
+	int poopCount = 0;
 
-          //ROS_INFO("rainy");
-        }else if (count%59){
-          status.data="Sunny";
-          //ROS_INFO("sunny");
-          Weather_publisher.publish(status);
+	////messages
+	//velocity of this RobotNode
+	std_msgs::String status;
+	status.data="Sunny";
+	while (ros::ok())
+	{
+		if (count%30==0)
+		{
+			status.data="Rainy";
+			Weather_publisher.publish(status);
 
-        }
+			//ROS_INFO("rainy");
+		}else if (count%59){
+			status.data="Sunny";
+			//ROS_INFO("sunny");
+			Weather_publisher.publish(status);
 
-        ros::spinOnce();
+		}
 
-        loop_rate.sleep();
-        count=count+1;
-  }
+		if (count % 100) {
+			if (poopCount < 5) {
+				poopInfo->data = "Request";
+				poopAdvert.publish(poopInfo);
+				ROS_INFO("Pooped appx: [%s]", poopInfo->data.c_str());
+			}
+			poopCount++;
+		}
 
-  return 0;
+		ros::spinOnce();
+
+		loop_rate.sleep();
+		count=count+1;
+	}
+
+	return 0;
 
 }
